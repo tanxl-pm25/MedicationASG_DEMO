915d346e909ea7da2a14e3061b39fc372af951c9
Xin Ling <tanxl-pm25@student.tarc.edu.my>
2026-07-29 23:08:01 +0800

    Initial Commit

--- app/src/main/java/com/example/medication_demo/MainActivity.kt
+++ app/src/main/java/com/example/medication_demo/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Medication_DemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
